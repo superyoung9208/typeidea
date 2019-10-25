9a3154207adf3b27604babe4da86f2543d9b0dfa
xwp_fullstack@163.com <491127805@qq.com>
2019-10-25 11:02:58 +0800

    升级为类视图

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,39 +1,117 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.views.generic import DetailView
+from django.views.generic import ListView
+
 from blog.models import Tag, Post, Category
 from config.models import Sidebar
 
 
-def post_list(request, category_id=None, tag_id=None):
-    tag = None
-    category = None
-    if tag_id:
-        post_list, tag = Post.get_by_tag(tag_id)
-    elif category_id:
-        post_list, category = Post.get_by_category(category_id)
-    else:
-        post_list = Post.latest_posts()
+# def post_list(request, category_id=None, tag_id=None):
+#     tag = None
+#     category = None
+#     if tag_id:
+#         post_list, tag = Post.get_by_tag(tag_id)
+#     elif category_id:
+#         post_list, category = Post.get_by_category(category_id)
+#     else:
+#         post_list = Post.latest_posts()
+#
+#     context = {
+#         'category': category,
+#         'tag': tag,
+#         'post_list': post_list,
+#         'sidebars': Sidebar.get_all()
+#
+#     }
+#     context.update(Category.get_navs())
+#     return render(request, 'blog/list.html', context=context)
 
-    context = {
-        'category': category,
-        'tag': tag,
-        'post_list': post_list,
-        'sidebars': Sidebar.get_all()
+class CommonViewMixin(object):
+    """通用扩展类"""
 
-    }
-    context.update(Category.get_navs())
-    return render(request, 'blog/list.html', context=context)
+    def get_context_data(self, **kwargs):
+        context = super(CommonViewMixin, self).get_context_data(**kwargs)
+        context.update({
+            'sidebars': Sidebar.get_all()
+        })
+        context.update(Category.get_navs())
+        return context
 
 
-def post_detail(request, post_id):
-    try:
-        post = Post.objects.get(id=post_id)
-    except Post.DoesNotExist:
-        post = None
-    context = {
-        'post': post,
-    }
-    context.update(Category.get_navs())
-    return render(request, 'blog/detail.html', context={'post': post})
+class IndexView(CommonViewMixin, ListView):
+    """首页视图"""
+    queryset = Post.latest_posts()
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
+
+
+class PostListView(ListView):
+    """列表页类视图"""
+    queryset = Post.latest_posts()
+    paginate_by = 1
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
+
+
+# def post_detail(request, post_id):
+#     try:
+#         post = Post.objects.get(id=post_id)
+#     except Post.DoesNotExist:
+#         post = None
+#     context = {
+#         'post': post,
+#     }
+#     context.update(Category.get_navs())
+#     return render(request, 'blog/detail.html', context={'post': post})
+
+
+class PostDetailView(CommonViewMixin,DetailView):
+    """博客详情页"""
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
+
+
+class CategoryView(IndexView):
+    """分类页"""
+
+    def get_context_data(self, **kwargs):
+        """获取当前的类型"""
+        context = super(CategoryView, self).get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+        })
+        return context
+
+    def get_queryset(self):
+        """重写queryset,根据分类过滤"""
+        queryset = super(CategoryView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    """标签页"""
+
+    def get_context_data(self, **kwargs):
+        """获取当前的类型"""
+        context = super(CategoryView, self).get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag': tag,
+        })
+        return context
+
+    def get_queryset(self):
+        """重写queryset,根据分类过滤"""
+        queryset = super(TagView, self).get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(category_id=tag_id)
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from blog.views import post_list, post_detail
+from blog.views import (IndexView, PostDetailView, TagView, CategoryView)
 from config.views import links
 
 urlpatterns = [
-    url(r'^$', post_list, name='index'),
-    url(r'^category/(?P<category_id>\d+)/$', post_list, name='category-list'),
-    url(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag-list'),
-    url(r'^post/(?P<post_id>\d+).html$', post_detail, name='post-detail'),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
+    url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
+    url(r'^post/(?P<pk>\d+).html$', PostDetailView.as_view(), name='post-detail'),
     url(r'^links/$', links),
     url(r'^admin/', admin.site.urls),
     # url(r'^super_admin/', admin.site.urls),
